0e1b732c64cc0381b2d3daa175def2334ce66982
Julian Badillo <j.badillo@numat-tech.com>
2024-02-15 11:23:00 -0600

    :test_tube: fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/react-in-jsx-scope */
 import './matchMedia.js';
+import React from 'react'
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // Unit tests for the app file
 describe('Index', () => {
   it('renders without crashing', () => {
-    render(<App />);
+    render(<React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    );
   });
 });
